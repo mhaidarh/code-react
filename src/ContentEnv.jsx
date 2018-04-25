45a90ed9555ef24c7c34856d09ceb3e00d4886c8
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-25 14:18:28 +0700

    Get data from API

--- src/ContentEnv.jsx
+++ src/ContentEnv.jsx
@@ -1,15 +1,39 @@
 import React from "react";
+import axios from "axios";
 
 const NAME = process.env.REACT_APP_NAME;
 const API_URL = process.env.REACT_APP_API_URL;
 
-const ContentEnv = () => {
-  return (
-    <div>
-      <h1>{NAME}</h1>
-      <a href={API_URL}>{API_URL}</a>
-    </div>
-  );
-};
+class ContentEnv extends React.Component {
+  state = {
+    data: []
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.getData = this.getData.bind(this);
+  }
+
+  getData() {
+    axios.get(`${API_URL}/todos`).then(res => {
+      this.setState({ data: res.data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 onClick={this.getData}>{NAME}</h1>
+        <a href={API_URL}>{API_URL}</a>
+        <ul>
+          {this.state.data.map((item, index) => {
+            return <li key={index}>{item.text}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default ContentEnv;
